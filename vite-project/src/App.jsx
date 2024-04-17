f2a8d6458415e79305286a0ece324826db1eb34a
Albin <albin.pilenas@gmail.com>
2024-04-17 10:58:15 +0200

    Co-authored-by: DanneN22 <DanneN22@users.noreply.github.com> Co-authored-by: Super-Victor2 <Super-Victor2@users.noreply.github.com>

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+import Header from './components/header';
+import Nav from './components/nav';
+import Main from './components/main';
+import Footer from './components/footer';
+
+import './index.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </div>
+  )
+  
+}
+
+export default App
