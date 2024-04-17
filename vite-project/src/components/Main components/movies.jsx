967ae7b7eecde01b6804a14ee5428dafd464169d
Victor <ninjavigge@gmail.com>
2024-04-17 17:58:44 +0200

    main komponenter färdig

--- vite-project/src/components/Main components/movies.jsx
+++ vite-project/src/components/Main components/movies.jsx
@@ -3,7 +3,7 @@ import './main.css';
 function Movies() {
     return (
         <section className="movies-container">
-            
+            <h1 className="movies-title">Topp 20</h1>
         </section>
     )
     }
