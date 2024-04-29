d78d34684ad18dc84f3f252f8c274df81c1948ba
Victor <ninjavigge@gmail.com>
2024-04-29 20:39:28 +0200

    watchList klar

--- vite-project/src/components/Home-components/movies.jsx
+++ vite-project/src/components/Home-components/movies.jsx
@@ -11,6 +11,7 @@ function MovieList() {
         try {
           const response = await axios.get('https://santosnr6.github.io/Data/movies.json');
           setMovies(response.data);
+          console.log(response)
         } catch (error) {
           console.error('Error fetching movies:', error);
         }
@@ -22,21 +23,44 @@ function MovieList() {
     return (
       <div className='movies-container'>
         {movies.map(movie => (
-          <MovieCard key={movie.id} movie={movie} />
+          <MovieCard key={movie.imdbid} movie={movie} />
         ))}
       </div>
     );
   }
   
   function MovieCard({ movie }) {
+    
+    const handleWatchListToggle = () => {
+      const storedMovies = JSON.parse(localStorage.getItem('watchList')) || [];
+      const movieIndex = storedMovies.findIndex(item => item.imdbid === movie.imdbid);
+  
+      let updatedList;
+      if (movieIndex !== -1) {
+          updatedList = storedMovies.filter((item) => item.imdbid !== movie.imdbid);
+      } else {
+          updatedList = [...storedMovies, movie];
+      }
+  
+      localStorage.setItem('watchList', JSON.stringify(updatedList));
+  };
+  
+  
+  
+
     return (
-      <>
-      <div className="movie-card">
-    <h2>{movie.title}</h2>
-    <img className="movie-poster" src={movie.poster} alt="" />         
-</div>
-      </>
+        <div className="movie-card">
+            <h2>{movie.title}</h2>
+            <div className="image-wrapper">
+                <i className="favStar fa-solid fa-star"></i>
+                <i className="watchPlus fa-solid fa-plus" onClick={handleWatchListToggle}></i>
+                <img className="movie-poster" src={movie.poster} alt="" /> 
+            </div>
+        </div>
     );
-  }
+}
+
+
+
   
   export default MovieList;
