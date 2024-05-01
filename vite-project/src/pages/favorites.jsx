a0c4cb87fdad8a47d6def9908dda37cfcb4e3b60
Albin <albin.pilenas@gmail.com>
2024-05-01 11:10:46 +0200

    final touch albin

--- vite-project/src/pages/favorites.jsx
+++ vite-project/src/pages/favorites.jsx
@@ -1,11 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import './pages.css';
 
-function favorites() {
+function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    setFavorites(storedFavorites);
+  }, []);
+
+  const toggleFavorite = (movieId) => {
+    const updatedFavorites = favorites.some(movie => movie.imdbid === movieId)
+      ? favorites.filter(movie => movie.imdbid !== movieId)
+      : [...favorites, { imdbid: movieId }];
+
+    setFavorites(updatedFavorites);
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+  };
+
   return (
-    
-    <div>
-        <h2>Låtsas som att det ligger favoriter här som du sparat</h2>
-    </div>
-  )
+    <section className="favorites-section">
+      {favorites.length === 0 ? (
+        <div className="favorites-empty">
+          <p>Dina favoriter är tomma.</p>
+        </div>
+      ) : (
+        favorites.map(movie => (
+          <div className="movie-card" key={movie.imdbid}>
+            <h2>{movie.title}</h2>
+            <div className="image-wrapper">
+              <i 
+                className={`favStar fa-solid fa-star ${favorites.some(item => item.imdbid === movie.imdbid) ? 'active' : ''}`}
+                onClick={() => toggleFavorite(movie.imdbid)}
+              ></i>
+              <img className="movie-poster" src={movie.poster} alt="" /> 
+            </div>
+          </div>
+        ))
+      )}
+    </section>
+  );
 }
 
-export default favorites
+export default Favorites;
